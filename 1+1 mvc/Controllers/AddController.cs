00daf0c38a35ed0d05252e1cc8d289d47590c9e8
hans <545872067@qq.com>
2018-05-07 16:34:52 +0800

    添加项目文件。

---
+++ 1+1 mvc/Controllers/AddController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1_1_mvc.Controllers
+{
+    public class AddController : Controller
+    {
+        // GET: Add
+        public ActionResult jiafa()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Xiangjia(string a,string b)
+        {
+            int c=0;
+            c = Convert.ToInt32(a) + Convert.ToInt32(b);
+
+
+            return Json(c);
+        }
+        public JsonResult Xiangjian(string a,string b)
+        {
+            int c = 0;
+            c = Convert.ToInt32(a) - Convert.ToInt32(b);
+
+            return Json(c);
+        }
+
+        public JsonResult XiangCheng(string a,string b)
+        {
+            int c = 0;
+
+            c = Convert.ToInt32(a) * Convert.ToInt32(b);
+
+            return Json(c);
+        }
+    }
+}
